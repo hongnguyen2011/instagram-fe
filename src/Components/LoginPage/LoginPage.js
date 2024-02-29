c4400f5570431fd6a1376203c6e860e6ca3b6f64
Hồng Nguyên <95576426+hongnguyen2011@users.noreply.github.com>
2024-02-29 19:08:15 +0700

    implementing the SIgnUp Component

--- src/Components/LoginPage/LoginPage.js
+++ src/Components/LoginPage/LoginPage.js
@@ -6,6 +6,9 @@ import insta_logo from "../../images/logoinsta.png";
 import fb from "../../images/fb.png";
 import appstore from "../../images/app.png";
 import playstore from "../../images/play.png";
+import SignIn from "../SignIn/SignIn";
+import SignUp from "../SignUp/SignUp";
+
 class LoginPage extends Component {
   constructor(props) {
     super(props);
@@ -13,6 +16,11 @@ class LoginPage extends Component {
       isLogin: true,
     };
   }
+
+  changeLogin = () => {
+    if (this.state.isLogin) this.setState({ isLogin: false });
+    else this.setState({ isLogin: true });
+  };
 
   render() {
     return (
@@ -22,50 +30,70 @@ class LoginPage extends Component {
           <Grid item xs={6}>
             <div className="loginpage__main">
               <div>
-                <img src={inst_image} alt="Instagram" width="454px" />
+                <img src={inst_image} width="454px" />
               </div>
               <div>
                 <div className="loginpage_rightcomponent">
-                  <img
-                    className="loginpage__logo"
-                    src={insta_logo}
-                    alt="Instagram Logo"
-                  />
+                  <img className="loginpage__logo" src={insta_logo} />
+                  <div className="loginPage__signin">
+                    {this.state.isLogin ? <SignIn /> : <SignUp />}
+
+                    <div className="login__ordiv">
+                      <div className="login__dividor"></div>
+                      <div className="login__or">OR</div>
+                      <div className="login__dividor"></div>
+                    </div>
+
+                    <div className="login__fb">
+                      <img
+                        src={fb}
+                        width="15px"
+                        style={{ marginRight: "5px" }}
+                      />
+                      Log in with Facebook
+                    </div>
+                    <div className="login_forget"> Forgot password?</div>
+                  </div>
                 </div>
-                <div className="loginPage_signing">
-                  <input
-                    className="loginpage_text"
-                    type="text"
-                    placeholder="Phone number, username, or email"
-                  />
-                  <input
-                    className="loginpage_text"
-                    type="password"
-                    placeholder="Password"
-                  />
-                  <button className="login_button"> Log in </button>
+
+                <div className="loginpage__signupoption">
+                  {this.state.isLogin ? (
+                    <div className="loginPage__signin">
+                      Don't have an account?{" "}
+                      <span
+                        onClick={this.changeLogin}
+                        style={{ fontWeight: "bold", color: "#0395F6" }}
+                      >
+                        Sign up
+                      </span>
+                    </div>
+                  ) : (
+                    <div className="loginPage__signup">
+                      Have an account?{" "}
+                      <span
+                        onClick={this.changeLogin}
+                        style={{ fontWeight: "bold", color: "#0395F6" }}
+                      >
+                        Sign in
+                      </span>
+                    </div>
+                  )}
                 </div>
-                <div className="login_ordiv">
-                  <div className="login_dividor"></div>
-                  <div className="login_or">OR</div>
-                  <div className="login_dividor"></div>
-                </div>
-                <div className="login__fb">
-                  <img src={fb} width="15px" style={{ marginRight: "5px" }} />
-                  Log in with Facebook
-                </div>
-                <div className="login_forget"> Forgot password?</div>
-                <div className="loginPage_option">
-                  <img
-                    className="loginPage_dwimg"
-                    src={appstore}
-                    width="136px"
-                  />
-                  <img
-                    className="loginPage_dwimg"
-                    src={playstore}
-                    width="136px"
-                  ></img>
+
+                <div className="loginPage__downloadSection">
+                  <div>Get the app.</div>
+                  <div className="loginPage__option">
+                    <img
+                      className="loginPage_dwimg"
+                      src={appstore}
+                      width="136px"
+                    />
+                    <img
+                      className="loginPage_dwimg"
+                      src={playstore}
+                      width="136px"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
